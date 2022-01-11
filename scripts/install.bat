d51399b54d8e2350c239b9088bcc34c3ac29cdae
Plasny <pppasternak@gmail.com>
2022-01-11 20:37:12 +0100

    adding helper build script

---
+++ scripts/install.bat
@@ -0,0 +1,7 @@
+:: 32bit build
+cmake -G "Visual Studio 16 2019" -A Win32 -S . -B out\build32
+cmake --build out\build32 --config Debug
+
+:: 64bit build
+::cmake -G "Visual Studio 16 2019" -A x64 -S . -B out\build64
+::cmake --build out\build64 --config Debug
